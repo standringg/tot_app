e3c156a6e698ce4cd58a4ae6767af56b548497f0
Greg Stanring <standringg@gmail.com>
2014-07-24 11:29:51 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TotApp::Application.config.secret_key_base = '5bd00052fb098a0f942388cbeee6e5feda38b03603aea5107a6577eaa01651547d8b8964b2062724cca9b45f9550deaa438ea715160ba813a9b076ee48a6dab4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TotApp::Application.config.session_store :cookie_store, key: '_tot_app_session'
