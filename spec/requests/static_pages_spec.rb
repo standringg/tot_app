5d92a8cc2f94f66653a9b8bfa8e89fb0c336c1ac
Greg Stanring <standringg@gmail.com>
2014-07-24 16:12:24 +1200

    Added a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,23 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Tour of Tauranga" }
+
   describe "Home page" do
 
-    it "should have the content 'Home'" do
+    it "should have the content 'Tour of Tauranga'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Home')
+      expect(page).to have_content('Tour of Tauranga')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Tour of Tauranga | Home")
+      expect(page).to have_title("Tour of Tauranga")
+    end
+
+   it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -24,7 +31,7 @@ describe "Static pages" do
 
     it "should have the title 'Event Details'" do
       visit '/static_pages/event_details'
-      expect(page).to have_title("Tour of Tauranga | Event Details")
+      expect(page).to have_title("#{base_title} | Event Details")
     end
   end
 
@@ -37,7 +44,7 @@ describe "Static pages" do
 
     it "should have the title 'Course'" do
       visit '/static_pages/course'
-      expect(page).to have_title("Tour of Tauranga | Course")
+      expect(page).to have_title("#{base_title} | Course")
     end
   end
 
@@ -50,7 +57,7 @@ describe "Static pages" do
 
   	it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Tour of Tauranga | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 end
