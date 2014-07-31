f1bc8759861f6d0bc6f9fa693d04df3d19843a24
Greg Stanring <standringg@gmail.com>
2014-07-31 14:56:43 +1200

    finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730050743) do
+ActiveRecord::Schema.define(version: 20140731022619) do
 
   create_table "users", force: true do |t|
     t.string   "user_name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140730050743) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,19 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when user_name is not present" do
     before { @user.user_name = " " }
@@ -113,4 +124,6 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+  
 end
